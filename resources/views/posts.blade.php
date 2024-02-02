daf535835400e8ac0a1988c68a712531fe4bc4df
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-02 11:53:15 +0200

    added layout using blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,17 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'mb-6' : '' }}">
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
 
-@foreach ($posts as $post)
-<article class="{{ $loop->even ? 'mb-6' : '' }}">
-    <h1>
-        <a href="/posts/{{ $post->slug }}">
-            {{ $post->title }}
-        </a>
-    </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+    @endforeach
 
-    <div>
-        {{ $post->excerpt }}
-    </div>
-</article>
-@endforeach
-
-@endsection
+</x-layout>
