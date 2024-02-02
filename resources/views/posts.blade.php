0edef69b3f86a4091d0680668af4afd44e5ffa3d
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-02 11:19:12 +0200

    updated the blades to use blade sytax instead of php

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,19 +9,19 @@
 </head>
 
 <body>
-    <?php foreach ($posts as $post): ?>
-    <article>
+    @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'mb-6' : '' }}">
         <h1>
-            <a href="/posts/<?= $post->slug; ?>">
-                <?= $post->title; ?>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
             </a>
         </h1>
 
         <div>
-            <?= $post->excerpt; ?>
+            {{ $post->excerpt }}
         </div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
 
 </html>
