a8fa333d1b28a01662c8b2c8f4f278cb53c49298
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-14 15:53:41 +0200

    mass assignment rules (3 ways)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    // ^ v1) set the guarded fields which CAN NOT be filled by the user
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    // ^ v2) set the fields which CAN be filled by the user
+    // protected $guarded = [];
+    // v3) !!! disable mass asignment restriction
 }
