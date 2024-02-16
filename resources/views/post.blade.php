cb38278b126ce8a9d77379c1d8c537c59159f22c
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-16 18:56:15 +0200

    prettier update after fix

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,9 @@
             By
             <a href="#">{{$post->user->name}}</a>
             in
-            <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+            <a href="/categories/{{$post->category->slug}}">
+                {{$post->category->name}}
+            </a>
         </p>
 
         <div>{!! $post->body; !!}</div>
