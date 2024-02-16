88984fdc4a553618c52eba6bf1103d555c78c744
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-16 16:40:29 +0200

    added show posts by category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,14 @@
 <x-layout>
-
     <article>
-        <h1>{!! $post->title; !!} </h1>
+        <h1>{!! $post->title; !!}</h1>
 
-        <div>
-            {!! $post->body; !!}
-        </div>
+        <a
+            href="/categories/{{$post->category->slug}}"
+            >{{$post->category->name}}</a
+        >
+
+        <div>{!! $post->body; !!}</div>
     </article>
 
     <a href="/">Go Back</a>
-
 </x-layout>
