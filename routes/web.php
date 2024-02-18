8aebb92e8357624f9b3c96016f8445c1c6821d80
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-18 13:54:58 +0200

    sort posts by latest

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
     });
 
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest('published_at')->with('category')->get()
     ]);
 });
 
