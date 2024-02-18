30a1d61b8856eaccb6d674cc85206698e96f2742
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-18 14:03:17 +0200

    small tweak to use author instead of user (changed the model to declare a different name than the database column name)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 
         <p>
             By
-            <a href="#">{{$post->user->name}}</a>
+            <a href="#">{{$post->author->name}}</a>
             in
             <a href="/categories/{{$post->category->slug}}">
                 {{$post->category->name}}
