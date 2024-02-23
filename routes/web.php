f838ba0faee69020ee719489793b0a9ac9cd2f39
Voodoo30fps <voodoo30fps@gmail.com>
2024-02-23 15:50:19 +0200

    named routes + extracted components for dropdown

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
         'posts' => Post::latest('published_at')->with(['category', 'author'])->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) { // Post::where('slug', $post)->firstOrFail();
     return view('post', [
@@ -42,7 +42,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
